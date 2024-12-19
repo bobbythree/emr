cbd8601ecdbc33df5de2340c56410178b3993376
bobbythree <mynameisbobbylewis@hotmail.com>
2024-12-18 18:26:14 -0600

    WIP layout,svelte

---
+++ src/routes/(with layout)/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { patients } from '$lib/patients';
+  import { page } from '$app/stores';
+  const routeName = $page.url.pathname.slice(1);
+</script>
+
+{#each patients as patient}
+  {#if patient.mr == routeName}
+  <h1>{patient.name}</h1>    
+  {/if}
+{/each}
+
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
